3df4530167fe0d95a418b3379ec3a638f5ce2687
geoffn7 <geoffrey.nguyen9@gmail.com>
2022-06-25 03:11:34 -0400

    Implement player movement
    - Add input from user
    - Player movement dependent on input
    - Player object updated in level to move

--- src/player.py
+++ src/player.py
@@ -8,3 +8,34 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(
             '../graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
+
+        self.direction = pygame.math.Vector2()
+        self.speed = 5
+
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_UP]:
+            self.direction.y = -1
+        elif keys[pygame.K_DOWN]:
+            self.direction.y = 1
+        else:
+            self.direction.y = 0
+
+        if keys[pygame.K_RIGHT]:
+            self.direction.x = 1
+        elif keys[pygame.K_LEFT]:
+            self.direction.x = -1
+        else:
+            self.direction.x = 0
+
+    def move(self):
+        if self.direction.magnitude() != 0:
+            self.direction = self.direction.normalize()
+
+        self.rect.x += self.direction.x * self.speed
+        self.rect.y += self.direction.y * self.speed
+
+    def update(self):
+        self.input()
+        self.move()
